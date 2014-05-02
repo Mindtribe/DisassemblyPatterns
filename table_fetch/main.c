a2c6a075530c122fe29324901f0e712a63fee7e2
Jerry Ryle <jerry@mindtribe.com>
2014-05-02 12:41:19 -0700

    Switched table_fetch example to the more advanced PIC16LF1703 for more straightforward assembly. Shortened function name so that comments can fit in our blog's width.

--- table_fetch/main.c
+++ table_fetch/main.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-static uint8_t get_table_value(uint8_t inputValue);
+static uint8_t table_fetch(uint8_t inputValue);
 
 int main(int argc, char *argv[]) {
   int inputValue = 0;
   if (argc > 1) {
     inputValue = atoi((char const *)argv[1]);
   }
-  return (int)get_table_value(inputValue);
+  return (int)table_fetch(inputValue);
 }
 
-static uint8_t get_table_value(uint8_t inputValue) {
+static uint8_t table_fetch(uint8_t inputValue) {
   static uint8_t const table[] = {
     0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01,
     0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00
